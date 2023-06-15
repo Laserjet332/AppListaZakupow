25791eee8374930d484b89d3b7ec0a05f62a7476
PMajerczyk <pawel.majerczyk@onet.eu>
2023-06-15 15:32:33 +0200

    adding and removing elem

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ def add_list(notebook, list_entry):
     list_entry.delete(0, tk.END)
     frame = ttk.Frame(notebook)
     notebook.add(frame, text=list_name)
+    listbox = tk.Listbox(frame, width=75, height=20)
+    listbox.pack()
 
 
 def remove_list(notebook):
@@ -18,10 +20,33 @@ def remove_list(notebook):
         current_tab = notebook.select()
         notebook.forget(current_tab)
 
+
+def add_elem(notebook, elem_entry):
+
+    if notebook.tabs():
+        elem_name = elem_entry.get()
+        if not elem_name:
+            elem_name = "None"
+        elem_entry.delete(0, tk.END)
+        current_tab = notebook.select()
+        frame = notebook.nametowidget(current_tab)
+        listbox = frame.winfo_children()[0]
+        listbox.insert(tk.END, elem_name)
+
+
+def remove_elem(notebook):
+
+    current_tab = notebook.select()
+    frame = notebook.nametowidget(current_tab)
+    listbox = frame.winfo_children()[0]
+    index = listbox.curselection()
+    if index:
+        listbox.delete(index)
+
+
 """
 def main():
 
-    # main window
     window = tk.Tk()
     window.geometry("750x500")
     window.minsize(width=750, height=500)
@@ -29,16 +54,21 @@ def main():
     window.title("AppListaZakupow")
 
     list_entry = tk.Entry(window, width=30)
-    list_entry.pack()
-
     add_list_button = tk.Button(window, text="Dodaj listę", command=lambda: add_list(notebook, list_entry))
-    add_list_button.pack()
-
     remove_list_button = tk.Button(window, text="Usuń listę", command=lambda: remove_list(notebook))
+    list_entry.pack()
+    add_list_button.pack()
     remove_list_button.pack()
 
     notebook = ttk.Notebook(window)
     notebook.pack()
+
+    elem_entry = tk.Entry(window, width=30)
+    remove_elem_button = tk.Button(window, text="Usuń element z aktualnej listy", command=lambda: remove_elem(notebook))
+    add_elem_button = tk.Button(window, text="Dodaj element do aktualnej listy", command=lambda: add_elem(notebook, elem_entry))
+    remove_elem_button.pack(side=tk.BOTTOM)
+    add_elem_button.pack(side=tk.BOTTOM)
+    elem_entry.pack(side=tk.BOTTOM)
 
     window.mainloop()
 
