55cdd71fbf6ee20dc59e863a63fc34ef14864950
ligas10105 <ligass22@yahoo.com>
2023-06-19 17:07:56 +0200

    search and display function add

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import tkinter as tk
 from tkinter import ttk
+from tkinter import messagebox
 
 
 def add_list(notebook, list_entry):
-
-    list_name = list_entry.get()
+    list_name = list_entry.get("1.0", tk.END).strip()
     if not list_name:
         list_name = "None"
-    list_entry.delete(0, tk.END)
+    list_entry.delete("1.0", tk.END)
     frame = ttk.Frame(notebook)
     notebook.add(frame, text=list_name)
     listbox = tk.Listbox(frame, width=75, height=20)
@@ -15,14 +15,12 @@ def add_list(notebook, list_entry):
 
 
 def remove_list(notebook):
-
     if notebook.tabs():
         current_tab = notebook.select()
         notebook.forget(current_tab)
 
 
 def add_elem(notebook, elem_entry):
-
     if notebook.tabs():
         elem_name = elem_entry.get()
         if not elem_name:
@@ -35,7 +33,6 @@ def add_elem(notebook, elem_entry):
 
 
 def remove_elem(notebook):
-
     current_tab = notebook.select()
     frame = notebook.nametowidget(current_tab)
     listbox = frame.winfo_children()[0]
@@ -44,21 +41,50 @@ def remove_elem(notebook):
         listbox.delete(index)
 
 
-"""
-def main():
+def search_list(notebook, list_entry):
+    search_query = list_entry.get("1.0", tk.END).strip()
+    if notebook.tabs():
+        found = False
+        for tab in notebook.tabs():
+            tab_name = notebook.tab(tab, option="text")
+            if search_query.lower() in tab_name.lower():
+                notebook.select(tab)
+                found = True
+                break
 
+        if not found:
+            messagebox.showinfo("Search Result", "No matching list found.")
+    else:
+        messagebox.showinfo("Search Result", "No lists available.")
+
+
+def main():
     window = tk.Tk()
     window.geometry("750x500")
     window.minsize(width=750, height=500)
     window.maxsize(width=750, height=500)
     window.title("AppListaZakupow")
 
-    list_entry = tk.Entry(window, width=30)
-    add_list_button = tk.Button(window, text="Dodaj listę", command=lambda: add_list(notebook, list_entry))
-    remove_list_button = tk.Button(window, text="Usuń listę", command=lambda: remove_list(notebook))
-    list_entry.pack()
-    add_list_button.pack()
-    remove_list_button.pack()
+    entry_frame = tk.Frame(window)
+    entry_frame.pack()
+
+    list_entry = tk.Text(entry_frame, width=30, height=2)
+    list_entry.pack(side=tk.BOTTOM)
+
+    add_list_button = tk.Button(entry_frame, text="Dodaj listę", command=lambda: add_list(notebook, list_entry))
+    add_list_button.pack(side=tk.RIGHT)
+
+    remove_list_button = tk.Button(entry_frame, text="Usuń listę", command=lambda: remove_list(notebook))
+    remove_list_button.pack(side=tk.LEFT)
+
+    search_frame = tk.Frame(window)
+    search_frame.pack()
+
+    search_label = tk.Label(search_frame)
+    search_label.pack(side=tk.LEFT)
+
+    search_button = tk.Button(search_frame, text="Szukaj", command=lambda: search_list(notebook, list_entry))
+    search_button.pack(side=tk.RIGHT)
 
     notebook = ttk.Notebook(window)
     notebook.pack()
@@ -75,4 +101,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-"""
